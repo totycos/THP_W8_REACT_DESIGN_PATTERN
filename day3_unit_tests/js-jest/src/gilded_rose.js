a1b6d59ccca9fa756156196dac52bb862560824b
totycos <dv94@hotmail.fr>
2024-02-28 14:34:33 +0100

    Commit DESIGN_PATTERN day 3 - gildeRose: Fix Conjured items

--- day3_unit_tests/js-jest/src/gilded_rose.js
+++ day3_unit_tests/js-jest/src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,61 +7,82 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+      const currentItem = this.items[i];
+
+      this.updateNormalItem(currentItem);
+      this.updateSpecialItem(currentItem);
+
+      if (currentItem.name !== 'Sulfuras, Hand of Ragnaros') {
+        currentItem.sellIn -= 1;
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+
+      this.updateConjuredItem(currentItem)
+      this.updateExpiredItem(currentItem);
     }
 
     return this.items;
+  }
+
+  updateNormalItem(item) {
+    if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.quality > 0 && item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.quality -= 1;
+      }
+    }
+  }
+
+  updateSpecialItem(item) {
+    if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.quality < 50) {
+        item.quality += 1;
+
+        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+          this.updateBackstagePasses(item);
+        }
+      }
+    }
+  }
+
+  updateBackstagePasses(item) {
+    if (item.sellIn < 11 && item.quality < 50) {
+      item.quality += 1;
+    }
+
+    if (item.sellIn < 6 && item.quality < 50) {
+      item.quality += 1;
+    }
+  }
+
+  updateConjuredItem(item) {
+    if (item.quality > 0 && item.name.startsWith('Conjured')) {
+      item.quality -= 1;
+    }
+  }
+
+  updateExpiredItem(item) {
+    if (item.sellIn < 0) {
+      if (item.name !== 'Aged Brie') {
+        if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+          if (item.quality > 0 && item.name !== 'Sulfuras, Hand of Ragnaros') {
+            item.quality -= 1;
+          }
+        } else {
+          item.quality = 0;
+        }
+      } else if (item.quality < 50) {
+        item.quality += 1;
+      }
+    }
   }
 }
 
 module.exports = {
   Item,
   Shop
-}
+};
